c2451d50cc997c6b3f5ac0bd2ab0f1b0c752ca2f
pranithkallakuri <f20180249@hyderabad.bits-pilani.ac.in>
2021-03-22 20:10:03 +0530

    Added Documentation

--- contour.hpp
+++ contour.hpp
@@ -3,6 +3,27 @@
 #include <set>
 #include <vector>
 #include "utility.hpp"
+/** @file */
+
+/**
+ * @brief
+ * 
+ * @param[in] h A single horizontal rectangle edge
+ * @param[in] S The list of stripes
+ * @param[in] bottom_int The x-coordinates of all the bottom edges of each stripe
+ * @param[in] top_int The x-coordinates of all the top edges of each stripe
+ * 
+ * @returns A set of intervals that are part of the contour of the input rectangles
+ */
 std::set<interval> contour_pieces(edge h, std::vector<stripe> S, std::vector<long long int> bottom_int, std::vector<long long int> top_int);
+
+/**
+ * @brief 
+ * 
+ * @param[in] H The set of horizontal rectangle edges.
+ * @param[in] S The list of stripes computed from the function "rectangle_dac"
+ * 
+ * @returns A set of intervals that denote the horizontal edges present on the contour of the input rectangles
+ */
 std::set<interval> contour(std::set<edge> H, std::vector<stripe> S);
 #endif
--- measure.hpp
+++ measure.hpp
@@ -4,6 +4,13 @@
 #include "utility.hpp"
 #include <set>
 
+/**
+ * @brief The function "measure" computes the union of the area of all rectangles. That is, it computes the total area covered by the input iso-rectangles
+ * 
+ * @param[in] S The list of stripes that are created from the input list of rectangles.
+ * 
+ * @returns The total area covered by the iso-rectangles. 
+ */
 unsigned long long int measure(std::vector<stripe> S);
 
 #endif
